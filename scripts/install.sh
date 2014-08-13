ae20e102928c5ac3eddc4573d37e0bfc51b73a31
yzzhu <zhuyouzhi03@163.com>
2014-08-13 17:38:15 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,24 @@
 #!/bin/bash -ex
 
 # Detect RVM
+# cf的ruby环境采用rbenv管理，如果系统已安装rvm,会提示卸载，卸载后再重新执脚本
 if (rvm >/dev/null 2>&1); then
     echo "Found RVM is installed! RVM is not supported by this installer. Remove it and rerun this script."
     exit 1
 fi
 
+#更新软件包列表 
 sudo apt-get update
 
+
+#执行intall_ruby.sh脚本，安装ruby相关环境，并使环境变量生效
 ./scripts/install_ruby.sh
 source ~/.profile
 
+#执行clone_nise_bosh.sh脚本，获取nise bosh 源文件
 ./scripts/clone_nise_bosh.sh
+
+#执行clone_cf_release.sh脚本，获取
 ./scripts/clone_cf_release.sh
 
 ./scripts/install_environemnt.sh
