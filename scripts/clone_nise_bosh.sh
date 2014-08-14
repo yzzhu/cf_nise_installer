da789decd599a9241cb0ee9e63e08b1b5c08f44e
yzzhu <zhuyouzhi03@163.com>
2014-08-14 15:07:34 +0800

    Update clone_nise_bosh.sh

--- scripts/clone_nise_bosh.sh
+++ scripts/clone_nise_bosh.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ex
 
+#nise bosh作为一个submodule挂在cf_nise_installer的github项目中，
+#clone_nise_bosh.sh这个脚本用于获取nis bos的源文件
+
 if [ ! "$(ls -A nise_bosh)" ]; then
     git submodule update --init --recursive nise_bosh
 
