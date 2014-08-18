11f9a21d875c8ed8d5017d64106230d93ec62be7
yzzhu <zhuyouzhi03@163.com>
2014-08-18 17:30:13 +0800

    Update install_cf_release.sh

--- scripts/install_cf_release.sh
+++ scripts/install_cf_release.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -ex
 
+#若没有设置NISE_IP_ADDRESS，或其值为空，则获取本ip做为NISE_IP_ADDRESS
 NISE_IP_ADDRESS=${NISE_IP_ADDRESS:-`ip addr | grep 'inet .*global' | cut -f 6 -d ' ' | cut -f1 -d '/' | head -n 1`}
 
+#执行generate_deploy_manifest.sh，该脚本根据template.yml生成deploy.yml,deploy.yml为部署时需要的配置文件
 ./scripts/generate_deploy_manifest.sh
 
 (
