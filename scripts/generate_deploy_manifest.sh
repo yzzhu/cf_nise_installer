face551607b93a16ccce4206b5a9e85e7f0a4f47
yzzhu <zhuyouzhi03@163.com>
2014-08-19 16:18:17 +0800

    Update generate_deploy_manifest.sh

--- scripts/generate_deploy_manifest.sh
+++ scripts/generate_deploy_manifest.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -ex
 
+#如果没有指定NISE_IP_ADDRESS，则取本ip为NISE_IP_ADDRESS
 NISE_IP_ADDRESS=${NISE_IP_ADDRESS:-`ip addr | grep 'inet .*global' | cut -f 6 -d ' ' | cut -f1 -d '/' | head -n 1`}
 
 sed "s/192.168.10.10/${NISE_IP_ADDRESS}/g" manifests/template.yml > manifests/deploy.yml
 
+#替换域名
 if [ "${NISE_DOMAIN}" != "" ]; then
     if (! sed --version 1>/dev/null 2>&1); then
         # not a GNU sed
@@ -13,6 +15,7 @@ if [ "${NISE_DOMAIN}" != "" ]; then
     fi
 fi
 
+#替换密码
 if [ "${NISE_PASSWORD}" != "" ]; then
     if (! sed --version 1>/dev/null 2>&1); then
         # not a GNU sed
