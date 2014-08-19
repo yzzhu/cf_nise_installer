95e6a782423cb37136c0ea05a229fe3f9c476a8f
yzzhu <zhuyouzhi03@163.com>
2014-08-19 16:28:32 +0800

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,6 +3,7 @@
 sudo /var/vcap/bosh/bin/monit
 sleep 5
 
+#首先停止除psql外的所有进程
 for process in `sudo /var/vcap/bosh/bin/monit summary | tail -n +3 | cut -d ' ' -f 2 | sed "s/'//g" | grep -v postgres`; do
     sudo /var/vcap/bosh/bin/monit stop $process
 done
@@ -19,6 +20,7 @@ if (sudo /var/vcap/bosh/bin/monit summary | tail -n +3 | grep -c -E "stop pendin
   echo "Unable to stop processes"
 else
   echo "Now stopping postgres..."
+  #待其他所有进程停止成功再停止psql
   sudo /var/vcap/bosh/bin/monit stop postgres
   for ((i=0; i < 12; i++)); do
     if (sudo /var/vcap/bosh/bin/monit summary | tail -n +3 | grep -c -E -v "stop pending$" > /dev/null); then
