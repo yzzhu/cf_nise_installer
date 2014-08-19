e460db698d06cdd713c7b7b75379809dc58be906
yzzhu <zhuyouzhi03@163.com>
2014-08-19 16:23:48 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,7 @@
 sudo /var/vcap/bosh/bin/monit
 sleep 5
 
+#首先启动psql、nats服务
 for process in \
     postgres \
     nats
@@ -11,9 +12,10 @@ do
     sleep 30
 done;
 
+#启动其它所有模块
 sudo /var/vcap/bosh/bin/monit start all
 
-
+#等待所有进程启动成功
 echo "Waiting for all processes to start"
 for ((i=0; i < 120; i++)); do
     if ! (sudo /var/vcap/bosh/bin/monit summary | tail -n +3 | grep -v -E "running$"); then
@@ -27,6 +29,7 @@ if (sudo /var/vcap/bosh/bin/monit summary | tail -n +3 | grep -v -E "running$");
     exit 1
 fi
 
+#执行完毕，关闭调试信息，打印结果
 set +x
 echo "All processes have been started!"
 api_url=`grep srv_api_uri: ./manifests/deploy.yml | awk '{ print $2 }'`
