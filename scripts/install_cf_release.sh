66a38af4716cfb1b8686288ffdfb54e954580932
yzzhu <zhuyouzhi03@163.com>
2014-08-19 16:13:31 +0800

    Update install_cf_release.sh

--- scripts/install_cf_release.sh
+++ scripts/install_cf_release.sh
@@ -11,6 +11,10 @@ NISE_IP_ADDRESS=${NISE_IP_ADDRESS:-`ip addr | grep 'inet .*global' | cut -f 6 -d
     bundle install
 
     # Old spec format
+    #执行nise_bosh，开始cf部署过程
+    # -y 对部署过程中的所有交互都回答yes
+    # ../cf-release 指定安装包
+    # ../manifests/deploy.yml 指定部署的模版文件
     sudo env PATH=$PATH bundle exec ./bin/nise-bosh -y ../cf-release ../manifests/deploy.yml micro -n ${NISE_IP_ADDRESS}
     # New spec format, keeping the  monit files installed in the previous run
     sudo env PATH=$PATH bundle exec ./bin/nise-bosh --keep-monit-files -y ../cf-release ../manifests/deploy.yml micro_ng -n ${NISE_IP_ADDRESS}
